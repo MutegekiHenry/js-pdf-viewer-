f35d5a9eff0f97f8bfee26fcf21871e6f703f49d
morelmiles <morelmiles@gmail.com>
2020-04-19 16:08:36 +0300

    Added the page render

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const url = "..docs/pdf1.pdf";
+
+let pdfDoc = null,
+  pageNum = 1,
+  pageIsRendering = false,
+  pageNumIsPending = null;
+
+const scale = 1.5,
+  canvas = document.querySelector("#pdf-render"),
+  ctx = canvas.getContext("2d");
+
+//Render the page
+const renderPage = (num) => {
+  pageIsRendering = true;
+
+  //Get the page that you wanna view
+  pdfDoc.getPage(num).then((page) => {
+    //Set the scale
+    const viewport = page.getViewport({ scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderCtx = {
+      canvasContext: ctx,
+      viewport,
+    };
+
+    page.render(renderCtx).promise.then(() => {
+      pageIsRendering = true;
+
+      if (pageNumIsPending !== null) {
+        renderPage(pageNumIsPending);
+        pageNumIsPending = null;
+      }
+    });
+
+    //Output the current page
+    document.querySelector("#page-num").textContent = num;
+  });
+};
